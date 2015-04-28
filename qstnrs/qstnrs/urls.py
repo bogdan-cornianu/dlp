556865f4f14ce6a01035374fe6b38ae0a898bc83
Bogdan Cornianu <bogdan.cornianu@3pillarglobal.com>
2015-04-28 18:17:51 +0300

    redirect to index.html on /

--- qstnrs/qstnrs/urls.py
+++ qstnrs/qstnrs/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'^$', 'questionnaire.views.index'),
     url(r'^qstnrs/', include('questionnaire.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
