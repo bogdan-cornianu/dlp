17ab66090fd99cd6f0aa6706ce1b8a5bb2ca5702
Bogdan Cornianu <bogdan.cornianu@3pillarglobal.com>
2015-05-12 18:19:49 +0300

    fix declaration for required packages.

--- qstnrs/setup.py
+++ qstnrs/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='Questionnaire',
     version='0.1',
-    packages=find_packages(),
+    packages=['questionnaire'],
     include_package_data=True,
     description='Simple questionnaire app.',
-    author='Bogdan',
-    requires=['django']
+    author='Bogdan Cornianu',
+    install_requires=['Django==1.4.20', 'MySQL-python==1.2.5', 'South==0.7.6']
 )
