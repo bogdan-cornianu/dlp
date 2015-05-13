ab73508451e3159168ac0cde85f7e3b9fbdab865
Bogdan Cornianu <bogdan.cornianu@3pillarglobal.com>
2015-05-13 18:36:29 +0300

    add South.

--- qstnrs/qstnrs/settings.py
+++ qstnrs/qstnrs/settings.py
@@ -121,8 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
+    'south',
     'questionnaire'
-    # 'south'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
