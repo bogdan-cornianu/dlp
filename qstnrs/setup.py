28d6e91a713ee6e6782c6328c1fc7d0e92d3212b
Bogdan Cornianu <bogdan.cornianu@3pillarglobal.com>
2015-05-13 18:32:57 +0300

    add pytest; modify tox to work woth pytest; fix dependencies in setup.py

--- qstnrs/setup.py
+++ qstnrs/setup.py
@@ -7,5 +7,12 @@ setup(
     include_package_data=True,
     description='Simple questionnaire app.',
     author='Bogdan Cornianu',
-    install_requires=['Django==1.4.20', 'MySQL-python==1.2.5', 'South==0.7.6']
+    install_requires=[
+        'Django==1.4.20',
+        'MySQL-python==1.2.5',
+        'South==1.0.2',
+        'pytest==2.7.0',
+        'pytest-django==2.8.0',
+        'tox==1.9.2'
+    ]
 )
