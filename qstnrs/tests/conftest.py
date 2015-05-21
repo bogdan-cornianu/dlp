c4721065d775dc58438a5788dfeaf94b79bf5a9c
Bogdan Cornianu <bogdan.cornianu@3pillarglobal.com>
2015-05-21 17:55:28 +0300

    Remove manually setting of id when creating pages, questions and answers.

--- qstnrs/tests/conftest.py
+++ qstnrs/tests/conftest.py
@@ -1,221 +1,72 @@
-from questionnaire.models import Questionnaire, Page, Question, Answer
+from questionnaire.models import Questionnaire, Answer
 import pytest
 
 
 @pytest.fixture(autouse=True)
 def set_up(transactional_db):
     # create Questionnaire
-    Questionnaire.objects.create(
+    q1 = Questionnaire.objects.create(
         questionnaire_name="Questionnaire_1",
         questionnaire_description="Questionnaire 1"
     )
 
-    Questionnaire.objects.create(
+    q2 = Questionnaire.objects.create(
         questionnaire_name="Questionnaire_2",
         questionnaire_description="Questionnaire 2"
     )
 
-    # create pages
-    Page.objects.create(
-        questionnaire_id=1,
-        page_name="Page1_Q1",
-        page_order=1
-    )
-    Page.objects.create(
-        questionnaire_id=1,
-        page_name="Page2_Q1",
-        page_order=2
-    )
-    Page.objects.create(
-        questionnaire_id=1,
-        page_name="Page3_Q1",
-        page_order=3
-    )
+    # Create pages
+    p1_qstnr1 = q1.page_set.create(page_name="Page1_Q1")
+    p2_qstnr1 = q1.page_set.create(page_name="Page2_Q1")
+    p3_qstnr1 = q1.page_set.create(page_name="Page3_Q1")
 
-    Page.objects.create(
-        questionnaire_id=2,
-        page_name="Page1_Q2",
-        page_order=1
-    )
+    q2.page_set.create(page_name="Page1_Q2")
 
     # create questions
-    Question.objects.create(
-        page_id=1,
-        question_text="What's your name?",
-        question_order=1
-    )
-    Question.objects.create(
-        page_id=1,
-        question_text="What's your age?",
-        question_order=2
-    )
-    Question.objects.create(
-        page_id=1,
-        question_text="What's your favorite color?",
-        question_order=3
-    )
-    Question.objects.create(
-        page_id=3,
-        question_text="What car do you drive?",
-        question_order=1
-    )
-    Question.objects.create(
-        page_id=3,
-        question_text="What's your favorite food?",
-        question_order=2
-    )
-    Question.objects.create(
-        page_id=3,
-        question_text="Where do you go for lunch?",
-        question_order=3
-    )
-    Question.objects.create(
-        page_id=2,
-        question_text="What flowers do you like?",
-        question_order=1
-    )
-    Question.objects.create(
-        page_id=2,
-        question_text="What animals do you like?",
-        question_order=2
-    )
+    name_qstn = p1_qstnr1.question_set.create(
+        question_text="What's your name?")
+    age_qstn = p1_qstnr1.question_set.create(question_text="What's your age?")
+    colors_qstn = p1_qstnr1.question_set.create(
+        question_text="What's your favorite color?")
+    flowers_qstn = p2_qstnr1.question_set.create(
+        question_text="What flowers do you like?")
+    animals_qstn = p2_qstnr1.question_set.create(
+        question_text="What animals do you like?")
+    cars_qstn = p3_qstnr1.question_set.create(
+        question_text="What car do you drive?")
+    food_qstn = p3_qstnr1.question_set.create(
+        question_text="What's your favorite food?")
+    lunch_qstn = p3_qstnr1.question_set.create(
+        question_text="Where do you go for lunch?")
 
     # create answers
-    Answer.objects.create(
-        answer_text="Vlad",
-        answer_score=-1,
-        question_id=1
-    )
-    Answer.objects.create(
-        answer_text="Mercedes",
-        answer_score=3,
-        question_id=4
-    )
-    Answer.objects.create(
-        answer_text="Lily",
-        answer_score=-2,
-        question_id=7
-    )
-    Answer.objects.create(
-        answer_text="12",
-        answer_score=2,
-        question_id=2
-    )
-    Answer.objects.create(
-        answer_text="Pizza",
-        answer_score=8,
-        question_id=5
-    )
-    Answer.objects.create(
-        answer_text="Dogs",
-        answer_score=1,
-        question_id=8
-    )
-    Answer.objects.create(
-        answer_text="Blue",
-        answer_score=-1,
-        question_id=3
-    )
-    Answer.objects.create(
-        answer_text="Drunken Rat",
-        answer_score=-3,
-        question_id=6
-    )
-    Answer.objects.create(
-        answer_text="Bogdan",
-        answer_score=10,
-        question_id=1
-    )
-    Answer.objects.create(
-        answer_text="Ion",
-        answer_score=2,
-        question_id=1
-    )
-    Answer.objects.create(
-        answer_text="BMW",
-        answer_score=-4,
-        question_id=4
-    )
-    Answer.objects.create(
-        answer_text="Logan",
-        answer_score=10,
-        question_id=4
-    )
-    Answer.objects.create(
-        answer_text="Lada",
-        answer_score=-2,
-        question_id=4
-    )
-    Answer.objects.create(
-        answer_text="Roses",
-        answer_score=-3,
-        question_id=7
-    )
-    Answer.objects.create(
-        answer_text="Anemone",
-        answer_score=5,
-        question_id=7
-    )
-    Answer.objects.create(
-        answer_text="Aster",
-        answer_score=6,
-        question_id=7
-    )
-    Answer.objects.create(
-        answer_text="Azalea",
-        answer_score=3,
-        question_id=7
-    )
-    Answer.objects.create(
-        answer_text="27",
-        answer_score=10,
-        question_id=2
-    )
-    Answer.objects.create(
-        answer_text="87",
-        answer_score=-6,
-        question_id=2
-    )
-    Answer.objects.create(
-        answer_text="Shaorma",
-        answer_score=5,
-        question_id=5
-    )
-    Answer.objects.create(
-        answer_text="Burger",
-        answer_score=-2,
-        question_id=5
-    )
-    Answer.objects.create(
-        answer_text="Cats",
-        answer_score=-4,
-        question_id=8
-    )
-    Answer.objects.create(
-        answer_text="Horses",
-        answer_score=9,
-        question_id=8
-    )
-    Answer.objects.create(
-        answer_text="Red",
-        answer_score=-3,
-        question_id=3
-    )
-    Answer.objects.create(
-        answer_text="Green",
-        answer_score=10,
-        question_id=3
-    )
-    Answer.objects.create(
-        answer_text="Iulius Mall",
-        answer_score=-5,
-        question_id=6
-    )
-    Answer.objects.create(
-        answer_text="The Note Pub",
-        answer_score=8,
-        question_id=6
-    )
+    name_qstn.answer_set.create(answer_text="Vlad", answer_score=-1)
+    name_qstn.answer_set.create(answer_text="Bogdan", answer_score=10)
+    name_qstn.answer_set.create(answer_text="Ion", answer_score=2)
+    age_qstn.answer_set.create(answer_text="12", answer_score=2)
+    age_qstn.answer_set.create(answer_text="27", answer_score=10)
+    age_qstn.answer_set.create(answer_text="87", answer_score=-6)
+    colors_qstn.answer_set.create(answer_text="Blue", answer_score=-1)
+    colors_qstn.answer_set.create(answer_text="Red", answer_score=-3)
+    colors_qstn.answer_set.create(answer_text="Green", answer_score=10)
+    flowers_qstn.answer_set.create(answer_text="Lily", answer_score=-2)
+    flowers_qstn.answer_set.create(answer_text="Roses", answer_score=-3)
+    flowers_qstn.answer_set.create(answer_text="Anemone", answer_score=5)
+    flowers_qstn.answer_set.create(answer_text="Aster", answer_score=6)
+    flowers_qstn.answer_set.create(answer_text="Azalea", answer_score=3)
+    animals_qstn.answer_set.create(answer_text="Dogs", answer_score=1)
+    animals_qstn.answer_set.create(answer_text="Cats", answer_score=-4)
+    animals_qstn.answer_set.create(answer_text="Horses", answer_score=9)
+    cars_qstn.answer_set.create(answer_text="Mercedes", answer_score=3)
+    cars_qstn.answer_set.create(answer_text="BMW", answer_score=-4)
+    cars_qstn.answer_set.create(answer_text="Logan", answer_score=10)
+    cars_qstn.answer_set.create(answer_text="Lada", answer_score=-2)
+    food_qstn.answer_set.create(answer_text="Pizza", answer_score=8)
+    food_qstn.answer_set.create(answer_text="Shaorma", answer_score=5)
+    food_qstn.answer_set.create(answer_text="Burger", answer_score=-2)
+    lunch_qstn.answer_set.create(answer_text="Drunken Rat", answer_score=-3)
+    lunch_qstn.answer_set.create(answer_text="Iulius Mall", answer_score=-5)
+    lunch_qstn.answer_set.create(answer_text="The Note Pub", answer_score=8)
 
 
 @pytest.fixture
--- qstnrs/tests/test_urls.py
+++ qstnrs/tests/test_urls.py
@@ -1,5 +1,6 @@
 import pytest
 from django.core.urlresolvers import reverse, NoReverseMatch
+from questionnaire.models import Questionnaire
 
 
 @pytest.mark.django_db
@@ -36,7 +37,11 @@ def test_index_url(client):
 def test_page_no_questions(client):
     """Test that a correct message is displayed when there are no questions
     for a given page."""
-    response = client.get(reverse('qstnrs-page', args=(2, 4)))
+    questionnaire = Questionnaire.objects.get(
+        questionnaire_name="Questionnaire_2")
+    page_id = questionnaire.page_set.all()[0].id
+    response = client.get(reverse('qstnrs-page',
+                                  args=(questionnaire.id, page_id)))
 
     assert response.status_code == 200
     assert "This page has no questions available." in response.content
